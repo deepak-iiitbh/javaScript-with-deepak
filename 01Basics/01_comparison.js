c47fbf8479d6350d3b25c77b8486c4276a54fa64
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-06-12 19:59:53 +0000

    comarison of null and undefined

---
+++ 01Basics/01_comparison.js
@@ -0,0 +1,13 @@
+console.log("02" > 1) ;
+console.log("02" > "1") ;
+
+console.log(null ==0) ;
+console.log(null > 0) ;
+console.log(null < 0) ;
+
+console.log(undefined == 0) ;
+console.log(undefined > 0) ;
+console.log(undefined < 0) ;
+
+//try to avoid camparision of null and undefined with other data types because it can lead to unexpected results.
+
