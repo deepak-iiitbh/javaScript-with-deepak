a0bc3d1670e137d432fcb45899963782b2269cac
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-06-12 12:51:22 +0000

    first variable lesson

---
+++ 01Basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountid = 1542455 ;
+let accountname = 'union bank of india' ;
+var accountlocation = "delhi" ;
+accountEmain = 'union@gmail.com' ;
+
+
+console.table([accountid , accountlocation , accountname , accountEmain]) ;
+
+//prefer not to use var datatype 
