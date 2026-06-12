fec87016b00620429dfc3aa3ec94bac010ec04a2
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-06-12 13:28:44 +0000

    learning of typeof variable in ja

---
+++ 01Basics/01_typeofvar.js
@@ -0,0 +1,13 @@
+//number 
+//bigint 
+//string 
+//boolean
+//null
+//undefined
+//symbol
+//object 
+
+console.log(typeof(null)) ;
+//type of null is consider as object 
+console.log(typeof(undefined)) ;
+//type of undefined is consider as undefined -> can be ask in interview 
