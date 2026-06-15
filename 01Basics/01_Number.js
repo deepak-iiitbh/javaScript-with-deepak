2385d48ac24d98d45e63e64e66c7c1ef81214ab1
deepak chaudhary <deepak.240101266@iiitbh.ac.in>
2026-06-15 19:08:19 +0000

    Add Number.js

---
+++ 01Basics/01_Number.js
@@ -0,0 +1,14 @@
+const num = 100 ;
+console.log(num) ;
+console.log(typeof num) ;
+
+const newnum = new Number(5545421.5451) //better way to declared 
+console.log(newnum ) ;
+console.log(newnum.toExponential(2)) ;
+console.log(newnum.toFixed(20)) ;
+
+console.log(newnum.toPrecision(10)) ;
+console.log(newnum.toLocaleString('en-US')) ;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
